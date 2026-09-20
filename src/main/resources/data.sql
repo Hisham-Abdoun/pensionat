52da575b6d27b475e61e87b1b06b39424093b17d
hishamabdoun <hishamabdoun1982@gmail.com>
2026-09-20 14:25:12 +0200

    Fix data.sql unique constraint using MERGE INTO and pass all tests

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,17 +1,23 @@
-INSERT INTO rooms (room_number, room_type, extra_beds, price_per_night)
-VALUES (101, 'ENKEL', 0, 500.0);
+MERGE INTO rooms (room_number, room_type, extra_beds, price_per_night)
+    KEY(room_number)
+    VALUES (101, 'ENKEL', 0, 500.0);
 
-INSERT INTO rooms (room_number, room_type, extra_beds, price_per_night)
-VALUES (102, 'DUBBEL', 0, 750.0);
+MERGE INTO rooms (room_number, room_type, extra_beds, price_per_night)
+    KEY(room_number)
+    VALUES (102, 'DUBBEL', 0, 750.0);
 
-INSERT INTO rooms (room_number, room_type, extra_beds, price_per_night)
-VALUES (103, 'DUBBEL', 1, 900.0);
+MERGE INTO rooms (room_number, room_type, extra_beds, price_per_night)
+    KEY(room_number)
+    VALUES (103, 'DUBBEL', 1, 980.0);
 
-INSERT INTO rooms (room_number, room_type, extra_beds, price_per_night)
-VALUES (104, 'ENKEL', 2, 1200.0);
+MERGE INTO rooms (room_number, room_type, extra_beds, price_per_night)
+    KEY(room_number)
+    VALUES (104, 'ENKEL', 2, 1200.0);
 
-INSERT INTO rooms (room_number, room_type, extra_beds, price_per_night)
-VALUES (105, 'ENKEL', 0, 1100.0);
+MERGE INTO rooms (room_number, room_type, extra_beds, price_per_night)
+    KEY(room_number)
+    VALUES (105, 'DUBBEL', 0, 1100.0);
 
-INSERT INTO rooms (room_number, room_type, extra_beds, price_per_night)
-VALUES (106, 'ENKEL', 0, 1000.0);
+MERGE INTO rooms (room_number, room_type, extra_beds, price_per_night)
+    KEY(room_number)
+    VALUES (106, 'ENKEL', 0, 1000.0);
